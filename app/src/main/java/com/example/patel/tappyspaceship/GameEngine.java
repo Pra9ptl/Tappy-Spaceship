07d6bb3198e04c34e648cce71484864df2b14d8c
Pra9_ptl <patelpranav1313@gmail.com>
2019-05-30 11:48:47 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/patel/tappyspaceship/GameEngine.java
@@ -0,0 +1,169 @@
+package com.example.patel.tappyspaceship;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.util.Random;
+
+public class GameEngine extends SurfaceView implements Runnable{
+
+    // Android debug variables
+    final static String TAG="TAPPY-SPACESHIP";
+
+    // screen size
+    int screenHeight;
+    int screenWidth;
+
+    // game state
+    boolean gameIsRunning;
+
+    // threading
+    Thread gameThread;
+
+
+    // drawing variables
+    SurfaceHolder holder;
+    Canvas canvas;
+    Paint paintbrush;
+
+
+
+    // -----------------------------------
+    // GAME SPECIFIC VARIABLES
+    // -----------------------------------
+
+    // ----------------------------
+    // ## SPRITES
+    // ----------------------------
+
+    // ----------------------------
+    // ## GAME STATS
+    // ----------------------------
+    int score = 0;
+
+    public GameEngine(Context context, int w, int h) {
+        super(context);
+
+
+        this.holder = this.getHolder();
+        this.paintbrush = new Paint();
+
+        this.screenWidth = w;
+        this.screenHeight = h;
+
+
+        this.printScreenInfo();
+
+        // @TODO: Add your sprites
+        // @TODO: Any other game setup
+
+    }
+
+
+    private void printScreenInfo() {
+
+        Log.d(TAG, "Screen (w, h) = " + this.screenWidth + "," + this.screenHeight);
+    }
+
+    private void spawnPlayer() {
+        //@TODO: Start the player at the left side of screen
+    }
+    private void spawnEnemyShips() {
+        Random random = new Random();
+
+        //@TODO: Place the enemies in a random location
+
+    }
+
+    // ------------------------------
+    // GAME STATE FUNCTIONS (run, stop, start)
+    // ------------------------------
+    @Override
+    public void run() {
+        while (gameIsRunning == true) {
+            this.updatePositions();
+            this.redrawSprites();
+            this.setFPS();
+        }
+    }
+
+
+    public void pauseGame() {
+        gameIsRunning = false;
+        try {
+            gameThread.join();
+        } catch (InterruptedException e) {
+            // Error
+        }
+    }
+
+    public void startGame() {
+        gameIsRunning = true;
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+
+    // ------------------------------
+    // GAME ENGINE FUNCTIONS
+    // - update, draw, setFPS
+    // ------------------------------
+
+    public void updatePositions() {
+        // @TODO: Update position of player
+        // @TODO: Update position of enemy ships
+        // @TODO: Collision detection between player and enemy
+    }
+
+    public void redrawSprites() {
+        if (this.holder.getSurface().isValid()) {
+            this.canvas = this.holder.lockCanvas();
+
+            //----------------
+
+            // configure the drawing tools
+            this.canvas.drawColor(Color.argb(255,0,0,0));
+            paintbrush.setColor(Color.WHITE);
+
+
+            //@TODO: Draw the player
+            //@TODO: Draw the enemy
+
+            //----------------
+            this.holder.unlockCanvasAndPost(canvas);
+        }
+    }
+
+    public void setFPS() {
+        try {
+            gameThread.sleep(120);
+        }
+        catch (Exception e) {
+
+        }
+    }
+
+    // ------------------------------
+    // USER INPUT FUNCTIONS
+    // ------------------------------
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int userAction = event.getActionMasked();
+        //@TODO: What should happen when person touches the screen?
+        if (userAction == MotionEvent.ACTION_DOWN) {
+            Log.d(TAG, "Person tapped the screen");
+        }
+        else if (userAction == MotionEvent.ACTION_UP) {
+            Log.d(TAG, "Person lifted finger");
+        }
+
+        return true;
+    }
+}
