54b03a9ae367898bd43751fc3ae04796229ca4d5
Pra9_ptl <patelpranav1313@gmail.com>
2019-06-03 10:17:57 -0400

    Defined properties and getter and setter of enemy class

--- app/src/main/java/com/example/patel/tappyspaceship/Enemy.java
+++ app/src/main/java/com/example/patel/tappyspaceship/Enemy.java
@@ -1,5 +1,68 @@
 package com.example.patel.tappyspaceship;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.graphics.Rect;
+
 public class Enemy {
 
+    //Properties
+    //image
+    //hitbox
+    private Bitmap enemyImage;
+    private Rect enemyHitBox;
+    private int xPosition;
+    private int yPosition;
+
+    public Enemy(Context context, int x, int y){
+        //1. set initial position of the enem
+        this.xPosition = x;
+        this.yPosition = y;
+
+        //2. Set the default imsge -- enemy has image
+        this.enemyImage = BitmapFactory.decodeResource(context.getResources(), R.drawable.alien_ship1);
+
+        // 3. se the default hitbox -- same hitbox
+        this.enemyHitBox = new Rect(
+                this.xPosition,
+                yPosition,
+                this.xPosition - 500 + this.enemyImage.getWidth(),
+                this.yPosition + this.enemyImage.getHeight());
+
+
+    }
+
+    public Bitmap getEnemyImage() {
+        return enemyImage;
+    }
+
+    public void setEnemyImage(Bitmap enemyImage) {
+        this.enemyImage = enemyImage;
+    }
+
+    public Rect getEnemyHitBox() {
+        return enemyHitBox;
+    }
+
+    public void setEnemyHitBox(Rect enemyHitBox) {
+        this.enemyHitBox = enemyHitBox;
+    }
+
+    public int getxPosition() {
+        return xPosition;
+    }
+
+    public void setxPosition(int xPosition) {
+        this.xPosition = xPosition;
+    }
+
+    public int getyPosition() {
+        return yPosition;
+    }
+
+    public void setyPosition(int yPosition) {
+        this.yPosition = yPosition;
+    }
 }
